f2e81937fdb00e72ee8a6caf1d241b23de1847bf
pohern <ohern_p@yahoo.com>
2021-03-26 16:58:57 -0500

    add longestRepeatingCharecterReplacment

--- problems.js
+++ problems.js
@@ -507,3 +507,32 @@ var lengthOfLongestSubstring = function (s) {
 };
 
 console.log(lengthOfLongestSubstring("abcabcbb") + ' times');
+
+// Longest Repeating Character Replacement
+
+var characterReplacement = function (s, k) {
+  let map = new Map(),
+    len = s.length,
+    max = 0,
+    repeatMax = 0,
+    windowStart = 0;
+
+  for (let windowEnd = 0; windowEnd < len; windowEnd += 1) {
+    let rightChar = s[windowEnd];
+
+    if (!map[rightChar]) map[rightChar] = 0;
+    map[rightChar] += 1;
+
+    repeatMax = Math.max(repeatMax, map[rightChar]);
+    while (windowEnd - windowStart + 1 - repeatMax > k) {
+      let leftChar = s[windowStart];
+      map[leftChar] -= 1;
+      windowStart++;
+    }
+
+    max = Math.max(max, windowEnd - windowStart + 1);
+  }
+  return max;
+};
+
+console.log(characterReplacement('ABAB', 2))
