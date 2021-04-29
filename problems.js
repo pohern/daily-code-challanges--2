8a4868afa7cfc8d396ebc5c664ffc8fcb4e2efab
pohern <ohern_p@yahoo.com>
2021-04-29 16:30:55 -0500

    add more problems from Hired

--- problems.js
+++ problems.js
@@ -652,3 +652,27 @@ function add(x) {
 const age = 25
 const myAge = add.call(age, 2)
 console.log(myAge)
+
+
+// Assesment to find largest in an array
+const solution = (numbers) => {
+  return Math.max(...numbers)
+};
+
+console.log(solution([7,2,6,3]))
+
+// A complete binary tree is a binary tree that each level except possibiliy the last level, is completed filled. Suppose you are giving a binary tree represented as an array. For example, [3, 6, 2, 9, -1, 10] retpresents the following binary tree, where -1 indicates it is a NULL node.
+const solution = (arr) => {
+  if (!arr) return "";
+  if (arr.length === 0) return "";
+  const sum = (arr, idx) => {
+    if (idx - 1 < arr.length) {
+      if (arr[idx - 1] === -1) return 0;
+      return arr[idx - 1] + sum(arr, idx * 2) + sum(arr, idx * 2 + 1);
+    }
+    return 0;
+  };
+  const left = sum(arr, 2);
+  const right = sum(arr, 3);
+  return left == right ? "" : left > right ? "Left" : "Right";
+};
